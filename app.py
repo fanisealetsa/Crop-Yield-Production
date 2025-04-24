dee7339ee6d82bfdaed7d8f9842d3d8d3f99ebee
fanisealetsa <159315624+fanisealetsa@users.noreply.github.com>
2025-04-24 14:55:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template, request, jsonify
-from PREDICTION MODEL RNII.ipynb import predict_yield
+from PREDICTION MODEL RNI import predict_yield
 
 app = Flask(__name__)
 
